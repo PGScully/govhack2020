7a5ae83a4ecc19aba56a7c464718091a7abeba6c
Peter Scully <peter@peterscully.name>
2020-08-15 21:32:29 +1000

    Add REST endpoints

--- mobile/lib/repository/rest_client.dart
+++ mobile/lib/repository/rest_client.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 
 import 'package:twp/repository/challenge.dart';
+import 'package:twp/repository/challenge_location.dart';
 
 part 'rest_client.g.dart';
 
@@ -11,4 +12,14 @@ abstract class RestClient {
 
   @GET("/challenges")
   Future<List<Challenge>> getChallenges();
+
+  @GET('/challenges/{id}')
+  Future<Challenge> getChallenge(@Path("id") String id);
+
+  @GET('/locations')
+  Future<List<ChallengeLocation>> getLocations();
+
+  @GET('/locations/{id}')
+  Future<List<ChallengeLocation>> getLocationsForChallenge(
+      @Path("id") String id);
 }
--- mobile/lib/repository/rest_client.g.dart
+++ mobile/lib/repository/rest_client.g.dart
@@ -33,4 +33,64 @@ class _RestClient implements RestClient {
         .toList();
     return value;
   }
+
+  @override
+  getChallenge(id) async {
+    ArgumentError.checkNotNull(id, 'id');
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        '/challenges/$id',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = Challenge.fromJson(_result.data);
+    return value;
+  }
+
+  @override
+  getLocations() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final Response<List<dynamic>> _result = await _dio.request('/locations',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    var value = _result.data
+        .map((dynamic i) =>
+            ChallengeLocation.fromJson(i as Map<String, dynamic>))
+        .toList();
+    return value;
+  }
+
+  @override
+  getLocationsForChallenge(id) async {
+    ArgumentError.checkNotNull(id, 'id');
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final Response<List<dynamic>> _result = await _dio.request('/locations/$id',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    var value = _result.data
+        .map((dynamic i) =>
+            ChallengeLocation.fromJson(i as Map<String, dynamic>))
+        .toList();
+    return value;
+  }
 }
